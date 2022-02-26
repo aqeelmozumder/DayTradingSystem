a2a2ee75de35bd793ae1e471ce8e05fc86a12bdc
scottlynch0 <scottlynch0@gmail.com>
2022-02-25 22:06:11 -0800

    userCollection

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import pymongo
 
 CLIENT = pymongo.MongoClient("mongodb://localhost:27017/")
 APP_DATABASE = CLIENT["seng468"]
-USER_COLLECTION = APP_DATABASE["config.USER_COLLECTION"]
+USER_COLLECTION = APP_DATABASE["config.userCollection"]
 
 QuoteServerHost = "127.0.0.1"
 QuoteServerPort = 65438

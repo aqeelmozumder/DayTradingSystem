da10f5988a7c4d0a3b4b1ffc25a5a4fd2da2c227
Junhong Deng <dengjunhong@gmail.com>
2022-05-15 03:52:33 -0700

    update init.sh

--- init.sh
+++ init.sh
@@ -7,3 +7,10 @@ docker-compose exec shard2_1 sh -c "mongo --port 27017 < /data/init-shard2.js"
 sleep 30
 
 docker-compose exec mongos sh -c "mongo --port 27017 < /data/db/init-router.js"
+
+# execute the WebServer.py and Client.py
+#docker-compose exec webserver sh -c "python3 /server/WebServer.py"
+
+#sleep(5)
+
+#docker-compose exec client sh -c "python3 /client/Client.py"
